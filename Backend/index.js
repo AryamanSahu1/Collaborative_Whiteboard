c0e1302acea98e8216770606057fa449ec3cc477
Aryaman Sahu <aryansahu7999@gmail.com>
2026-06-21 23:00:01 +0530

    url change

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cors({
     origin: [
         "http://localhost:3000",
-        "https://collaborative-whiteboard-f8yd-asm9qvpt9-aryaman-sahus-projects.vercel.app"
+        "https://collaborative-whiteboard-f8yd.vercel.app/"
     ]
 }));
 
@@ -35,7 +35,7 @@ const io = new Server(server, {
     cors: {
         origin: [
             "http://localhost:3000",
-            "https://collaborative-whiteboard-f8yd-asm9qvpt9-aryaman-sahus-projects.vercel.app"
+            "https://collaborative-whiteboard-f8yd.vercel.app/"
         ]
     }
 });
